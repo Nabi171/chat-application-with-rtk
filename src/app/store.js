ede0d81801bd886912196d1c404a5c08cb8919e7
Nabi171 <nabiultasbed@gmail.com>
2023-03-19 01:13:23 +0600

    34f34

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from '../features/api/apiSlice';
+import authSliceReducer from '../features/auth/authSlice';
+import conversationSliceReducer from '../features/conversations/conversationSlice';
 import counterReducer from '../features/counter/counterSlice';
+import messagesSliceReducer from '../features/messages/messagesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSliceReducer,
+    conversations: conversationSliceReducer,
+    messages: messagesSliceReducer,
   },
 });
