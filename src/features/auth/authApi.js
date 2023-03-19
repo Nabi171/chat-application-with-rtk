c024e9f9802cf206248428edae8de164844ce585
Nabi171 <nabiultasbed@gmail.com>
2023-03-19 07:59:58 +0600

    34fds34

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,7 +1,21 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const authApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => {
+    endpoints: (builder) => ({
         // endpoints here
-    },
+        register:builder.mutation({
+            query:(data)=>({
+                url:"/register",
+                method:"POST",
+                body:data,
+            })
+        }),
+        register:builder.mutation({
+            query:(data)=>({
+                url:"/register",
+                method:"POST",
+                body:data,
+            })
+        }),
+    }),
 });
