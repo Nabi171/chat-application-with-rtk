0b89081b3202b023c84476d46627108fbeed4ad0
Nabi171 <nabiultasbed@gmail.com>
2023-03-20 19:22:38 +0600

    c2gag

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import useAuthCheck from "./hooks/useAuthCheck";
 import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+
 function App() {
-    return (
+    const authChecked = useAuthCheck();
+
+    return !authChecked ? (
+        <div>Checking authentication....</div>
+    ) : (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
@@ -18,3 +24,4 @@ function App() {
 }
 
 export default App;
+
